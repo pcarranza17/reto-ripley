40e3e12124864d1c78dd60a322a3fff39fa5179b
pcarranza17 <pierinacarranza.17@outlook.com>
2021-02-12 10:56:15 -0500

    commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'reto-ripley';
+
+  items: MenuItem[];
+
+  ngOnInit() {
+      this.items = [
+          {label: 'Listado', icon: 'pi pi-fw pi-home', routerLink: ["home"]},
+          {label: 'Reporte', icon: 'pi pi-fw pi-image', routerLink: ["report"]}
+      ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+import { ToolbarModule } from 'primeng/toolbar';
+import {CardModule} from 'primeng/card';
+import {MessagesModule} from 'primeng/messages';
+import {MessageModule} from 'primeng/message';
+import {ChartModule} from 'primeng/chart';
+import {TabMenuModule} from 'primeng/tabmenu';
 
+// Componentes
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+
+// Rutas
+import { APP_ROUTING } from './app.routes';
+
+// Servicios
+import { ClientService } from './services/client.service';
+import { ReportComponent } from './components/report/report.component';
+import { NewClientComponent } from './components/new-client/new-client.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ReportComponent,
+    NewClientComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    ToolbarModule,
+    DialogModule,
+    CardModule,
+    MessagesModule,
+    MessageModule,
+    ChartModule,
+    TabMenuModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    ClientService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
